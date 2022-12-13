0fd1356224ccbe484b2e62446ae7175e35cc1daa
Metriximor <metriximor@gmail.com>
2022-12-13 00:25:41 +0000

    Add total conversion sandbox as a base

---
+++ python_scripts/generate_goods_modifier_types.py
@@ -0,0 +1,48 @@
+import re
+from pathlib import Path
+
+UTF_8_BOM = 'utf-8-sig'
+
+languages = ['english']
+generated_goods = []
+
+for file in Path('common/goods').glob('*.txt'):
+    output_file_path = Path(f"common/modifier_types/building_{file.stem}_modifier_types.txt")
+    output_file_path.parent.mkdir(exist_ok=True, parents=True)
+    with open(file, encoding=UTF_8_BOM) as goods_file, open(output_file_path, 'w+', encoding=UTF_8_BOM) as output_file:
+        for line in goods_file:
+            match = re.search(r"^\w+", line)
+            if match:
+                goods = match.group(0)
+                output_file.write(f'''building_output_{goods}_add = {{
+    good = yes
+    percent = no
+}}
+
+building_input_{goods}_add = {{
+    good = no
+    percent = no
+}}
+
+building_output_{goods}_mult = {{
+    good = yes
+    percent = yes
+}}
+
+''')
+                generated_goods.append(goods)
+            else:
+                continue
+    for language in languages:
+        localization_file_path = Path(f"localization/{language}/modifiers_l_{language}.yml")
+        localization_file_path.parent.mkdir(exist_ok=True, parents=True)
+        with open(localization_file_path, 'w+', encoding=UTF_8_BOM) as localization_file:
+            for goods in generated_goods:
+                # TODO Sanitize the second goods in each line for user readable text
+                localization_file.write(f''' modifier_building_input_{goods}_add:0 "@{goods}! {goods} input per level"
+ modifier_building_input_{goods}_add_desc:0 "The amount of @{goods}! {goods} consumed by buildings"
+ modifier_building_output_{goods}_add:0 "@{goods}! {goods} output per level"
+ modifier_building_output_{goods}_add_desc:0 "The amount of @{goods}! {goods} produced by buildings"
+ modifier_building_output_{goods}_mult:0 "Building @{goods}! {goods} output"
+ modifier_building_output_{goods}_mult_desc:0 "A bonus or penalty to the amount of @{goods}! {goods} produced by buildings"
+''')

0fc74484fb0a71aaff2312ece20b9fce7c0249d4
Metriximor <metriximor@gmail.com>
2022-12-14 15:47:43 +0000

    gitignore and bom script

--- src/convert_to_utf8bom.py
+++ src/convert_to_utf8bom.py
@@ -4,5 +4,18 @@ from pathlib import Path
 root_dir = "./"
 
 for txt_file in glob.iglob(root_dir + "/**/*.txt", recursive=True):
-    path = Path(txt_file)
-    path.write_text(path.read_text(), encoding="utf-8-sig")
+    with open(txt_file, 'rb') as f:
+        data = f.read(3)
+
+        if data == b'\xef\xbb\xbf':
+            continue
+        
+        f.seek(0)
+        data = f.read()
+
+    print(f"Converting {txt_file} to UTF-8-BOM")
+    text = data.decode('utf-8')
+    data = text.encode('utf-8-sig')
+
+    with open(txt_file, 'wb') as f:
+        f.write(data)

9bb3d7158246e24bdbb44910a59aa8242fb7e54d
Metriximor <metriximor@gmail.com>
2022-12-14 18:35:49 +0000

    add stone and wood tiers (#7)
    * add .gitignore
    
    * Change rule to not track individual settings file
    
    * Added python script to convert png to dds
    
    * edit+addons
    
    * gtgtg
    
    gtg
    
    * Fix metadata
    
    * new
    
    ew
    
    * ww
    
    w
    
    * tools
    
    tools
    
    * fix size tools
    
    * charcoal
    
    * resize plus more items
    
    * added village and updated textures
    
    * Change the library to convert png to dds, add example country
    
    * add stone texture
    
    * other background buildings
    
    * farm
    
    * added basic localization and icons to tool factory
    
    * state pictures
    
    * gitignore and bom script
    
    * Added mines and logging camps
    
    * added more methods for tools and such
    
    * new manual icon
    
    * me
    
    * Added stone tier factories
    
    Co-authored-by: Zeldrin <85764616+Zeldrin@users.noreply.github.com>

---
+++ src/convert_png_to_dds.py
@@ -0,0 +1,12 @@
+import glob
+from wand.image import Image
+from pathlib import Path
+
+root_dir = "./"
+
+for png_image in glob.iglob(root_dir + "gfx/interface/**/*.png", recursive=True):
+    with Image(filename=png_image) as open_image:
+        print(f"Converting {png_image} to .dds from .png")
+        open_image.compression='dxt5'
+        path = Path(png_image)
+        open_image.save(filename=path.with_suffix('.dds'))

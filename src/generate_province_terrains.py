f4269bdd6851050627d1c94385dfac0a3d48d26f
Metriximor <metriximor@gmail.com>
2023-01-14 21:33:22 +0000

    Make every single province terrain be plains

--- src/generate_province_terrains.py
+++ src/generate_province_terrains.py
@@ -26,6 +26,7 @@ def map_colors(array):
   return color_map
 
 color_map = {}
+# Provinces colors is a dict where the key is a tuple (r,g,b) and the values are the (x,y) locations
 provinces_path = "src/output/provinces_colors"
 if file_exists(provinces_path):
   with bz2.BZ2File(provinces_path, 'rb') as provinces_colors:
@@ -38,3 +39,11 @@ else:
     print(len(color_map))
     with bz2.BZ2File(provinces_path, 'wb') as config_dictionary_file:
         pickle.dump(color_map, config_dictionary_file)
+
+output = "#This is a generated file, do not modify unless you know what you are doing!\n"
+rgb2hex = lambda r,g,b: 'x%02X%02X%02X' %(r,g,b)
+for hex in [ rgb2hex(r, g, b) for r, g, b in color_map ]:
+    output += f'{hex}="plains"\n'
+
+with open("src/output/province_terrains.txt", 'w+', encoding=UTF_8_BOM) as output_file:
+    output_file.write(output)

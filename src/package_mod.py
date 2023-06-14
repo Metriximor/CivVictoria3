74e76bd874d95f01588d4caf6403e557fb970554
Metriximor <metriximor@gmail.com>
2023-06-14 14:05:38 +0100

    fix package script to actually work

--- src/package_mod.py
+++ src/package_mod.py
@@ -13,34 +13,28 @@ folders_to_not_copy: list[Path] = [
     Path("__pycache__"),
     Path("gfx/editor_terrain"),
     Path("content_source"),
-    Path("release")
+    Path("release"),
 ]
-disallowed_extensions = [
-    ".spec",
-    ".lnk",
-    ".psd",
-    ".pdn",
-    ".gitignore",
-    ".dvc"
-]
-disallowed_files = [
-    "tasklist.txt"
-]
-folders_to_keep_pngs: list[Path] = [
-    Path("gfx/map/terrain"),
-    Path("map_data")
-]
+disallowed_extensions = [".spec", ".lnk", ".psd", ".pdn", ".gitignore", ".dvc"]
+disallowed_files = ["tasklist.txt"]
+folders_to_keep_pngs: list[Path] = [Path("gfx/map/terrain"), Path("map_data")]
 
 files_to_package = []
 
 
-def compress_to_zip(source_folder: str, zip_file: str) -> None:
-    with zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zipf:
-        for root, _, files in os.walk(source_folder):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, arcname=os.path.relpath(
-                    file_path, source_folder))
+def compress_files_to_zip(file_list: list[str], zip_file_name: str) -> None:
+    total_files = len(file_list)
+    files_processed = 0
+
+    with zipfile.ZipFile(
+        zip_file_name, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9
+    ) as zipf:
+        for file_path in file_list:
+            zipf.write(file_path)
+            files_processed += 1
+            print(f'Progress: {files_processed}/{total_files} files compressed.')
+    
+        print('Compression completed.')
 
 
 def copy_files_to_release_folder(file_paths: list[str], release_folder: str) -> None:
@@ -51,27 +45,31 @@ def copy_files_to_release_folder(file_paths: list[str], release_folder: str) -> 
 for file in glob.iglob("./**/*", recursive=True):
     path = Path(file)
     is_disallowed_folder = any(
-        disallowed_folder == path for disallowed_folder in folders_to_not_copy)
+        disallowed_folder == path for disallowed_folder in folders_to_not_copy
+    )
     is_inside_disallowed_folder = any(
-        disallowed_folder in path.parents for disallowed_folder in folders_to_not_copy)
+        disallowed_folder in path.parents for disallowed_folder in folders_to_not_copy
+    )
     if is_inside_disallowed_folder or is_disallowed_folder:
         continue
-    has_disallowed_extension = any(file.endswith(
-        disallowed_extension) for disallowed_extension in disallowed_extensions)
+    has_disallowed_extension = any(
+        file.endswith(disallowed_extension)
+        for disallowed_extension in disallowed_extensions
+    )
     if has_disallowed_extension:
         continue
     file_is_png = path.suffix == ".png"
     png_is_in_allowed_folder = any(
-        allowed_folder in path.parents for allowed_folder in folders_to_keep_pngs)
+        allowed_folder in path.parents for allowed_folder in folders_to_keep_pngs
+    )
     if file_is_png and not png_is_in_allowed_folder:
         continue
-    is_disallowed_file = any(file.endswith(disallowed_file)
-                             for disallowed_file in disallowed_files)
+    is_disallowed_file = any(
+        file.endswith(disallowed_file) for disallowed_file in disallowed_files
+    )
     if is_disallowed_file:
         continue
     files_to_package.append(file)
 
 
-release_folder = "/release"
-copy_files_to_release_folder(files_to_package, release_folder)
-compress_to_zip(release_folder, f"{release_folder}/release.zip")
+compress_files_to_zip(files_to_package, "src/output/release.zip")
